a9c441ed05f73adc1d9a7650504f324eb1780991
Daniel Leussler <daniel@leussler.net>
2021-07-04 12:39:59 -0700

    adding a few comments, clarifying variable names

--- get_feature_nvdi.py
+++ get_feature_nvdi.py
@@ -9,12 +9,16 @@ from satsearch import Search
 def get_geo_feature():
 
     geojson_feature = requests.get('https://gist.githubusercontent.com/rodrigoalmeida94/369280ddccf97763da54371199a9acea/raw/d18cd1e266023d08464e13bf0e239ee29175e592/doberitzer_heide.geojson')
+
+    # Write the working json to a file, in case we want to reference it later
     file = open('geojson_tmp/workingfeature.json', "w")
     file.write(geojson_feature.text)
     file.close()
 
     return geojson_feature.json()
 
+
+# Query STAC with our geofeature coords
 def query_element84(api_endpoint, geofeature):
     search = Search(api_endpoint, intersects=geofeature)
     return search
@@ -26,9 +30,9 @@ if __name__ == "__main__":
     stac_api_endpoint = "https://earth-search.aws.element84.com/v0"
 
 
-    geo_feature = get_geo_feature()['features'][0]['geometry']
+    geo_feature_coords = get_geo_feature()['features'][0]['geometry']
 
     #print(geo_feature['features'][0]['geometry'])
 
-    results = query_element84(stac_api_endpoint, geo_feature)
+    results = query_element84(stac_api_endpoint, geo_feature_coords)
     print(results.found())
