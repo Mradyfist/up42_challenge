2f2d187c3d23049450c9bc8bf1a599072ab80285
Daniel Leussler <daniel@leussler.net>
2021-07-04 12:30:12 -0700

    test passing our coordinates to stac endpoint and getting search results

--- get_feature_nvdi.py
+++ get_feature_nvdi.py
@@ -7,6 +7,7 @@ from satsearch import Search
 
 # Get the described feature as the default, or whatever is passed as an arg
 def get_geo_feature():
+
     geojson_feature = requests.get('https://gist.githubusercontent.com/rodrigoalmeida94/369280ddccf97763da54371199a9acea/raw/d18cd1e266023d08464e13bf0e239ee29175e592/doberitzer_heide.geojson')
     file = open('geojson_tmp/workingfeature.json', "w")
     file.write(geojson_feature.text)
@@ -25,7 +26,9 @@ if __name__ == "__main__":
     stac_api_endpoint = "https://earth-search.aws.element84.com/v0"
 
 
-    geo_feature = get_geo_feature()
+    geo_feature = get_geo_feature()['features'][0]['geometry']
+
+    #print(geo_feature['features'][0]['geometry'])
 
     results = query_element84(stac_api_endpoint, geo_feature)
-    print(results)
+    print(results.found())
