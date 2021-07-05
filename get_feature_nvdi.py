173fea5939216f644aead3a8a1ca0183f8ad2ae7
Daniel Leussler <daniel@leussler.net>
2021-07-04 20:58:41 -0700

    Made the gist URL the default, but now a positional arg can be passed to the script via CLI to pull geometry from a different source

--- get_feature_nvdi.py
+++ get_feature_nvdi.py
@@ -1,17 +1,22 @@
 import requests
 import json
 import os
+import sys
 from satsearch import Search
 import numpy as np
 import cv2
 from osgeo import gdal
-import rasterio
+import rasterio, rasterio.mask
 
 
 # Get the described feature as the default, or whatever is passed as an arg
-def get_geo_feature():
+def get_geo_feature(feature_url):
 
-    geojson_feature = requests.get('https://gist.githubusercontent.com/rodrigoalmeida94/369280ddccf97763da54371199a9acea/raw/d18cd1e266023d08464e13bf0e239ee29175e592/doberitzer_heide.geojson')
+    try:
+        geojson_feature = requests.get(feature_url)
+    except:
+        print("Invalid geofeature URL")
+        sys.exit(1)
 
     # Write the working json to a file, in case we want to reference it later
     file = open('geojson_tmp/workingfeature.json', "w")
@@ -32,7 +37,9 @@ def query_element84(api_endpoint, geofeature, max_cloud_cover):
 
     return search
 
+# Function to calculate NDVI - possible that it needs tweaks to accommodate the data?
 def calc_ndvi(nir, red, offset):
+
     return (nir - red) / (nir + red + offset)
 
 #print(geojson_feature.json())
@@ -45,8 +52,8 @@ if __name__ == "__main__":
     # List where we can keep ndvi means as we calculate them per chunk
     per_chunk_ndvi_means = []
 
-
-    geo_feature_coords = get_geo_feature()['features'][0]['geometry']
+    feature_url = sys.argv[1] if len(sys.argv) > 1 else 'https://gist.githubusercontent.com/rodrigoalmeida94/369280ddccf97763da54371199a9acea/raw/d18cd1e266023d08464e13bf0e239ee29175e592/doberitzer_heide.geojson'
+    geo_feature_coords = get_geo_feature(feature_url)['features'][0]['geometry']
 
 
     results = query_element84(stac_api_endpoint, geo_feature_coords, 0)
@@ -66,34 +73,36 @@ if __name__ == "__main__":
             latest_images.append(item)
             print(image_gridsquares)
 
+    # Make lists of image refs for both red and NIR image sets, matched by index
     for item in latest_images:
         images_red.append(item.asset('red')['href'])
         images_nir.append(item.asset('nir')['href'])
 
-        # checking to make sure we're using correct bands
-        #print(item.asset('red'))
-
-
-
+    # iterate over one set, opening the matching image in sync
     for index, image in enumerate(images_red):
         with rasterio.open(image) as im_red:
             with rasterio.open(images_nir[index]) as im_nir:
 
-                # adding clip to eliminate negative values, to see if that's my normalization issue
-                overviews = im_red.overviews(1)
 
-                # select the second-largest resolution
+                # select the second-largest resolution, this could be changed via an argument but performance seems good where it's at
+                overviews = im_red.overviews(1)
                 overview_selected = overviews[1]
+
+                print(geo_feature_coords)
 
 
                 im_red_chunk = im_red.read(out_shape=(1, int(im_red.height // overview_selected), int(im_red.width // overview_selected))).clip(0)
                 im_nir_chunk = im_nir.read(out_shape=(1, int(im_nir.height // overview_selected), int(im_nir.width // overview_selected))).clip(0)
 
+                # Mask chunks based on original geofeature coords
+
+                #im_red_masked = rasterio.mask.mask(im_red_chunk, [geo_feature_coords], nodata=0)
+
                 ndvi = calc_ndvi(im_nir_chunk, im_red_chunk, 0.000001)
-                #print(ndvi)
                 mean_ndvi = ndvi.mean()
                 print(f"Image set {index} - mean NDVI: {mean_ndvi}")
                 per_chunk_ndvi_means.append(mean_ndvi)
+
     area_ndvi_mean = sum(per_chunk_ndvi_means) / len(per_chunk_ndvi_means)
 
     print(f"The mean NDVI for the given area is {area_ndvi_mean}")
